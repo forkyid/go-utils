067e27f5a47e5800e6a323a669454a7a8eecfc82
Heggies Iqbal <heggies0iqbal@gmail.com>
2020-10-19 14:10:02 +0700

    feat: auth middleware

---
+++ middleware/auth.go
@@ -0,0 +1,93 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/forkyid/go-utils/aes"
+	"github.com/forkyid/go-utils/cache"
+	"github.com/forkyid/go-utils/jwt"
+	"github.com/forkyid/go-utils/logger"
+	"github.com/forkyid/go-utils/rest"
+	"github.com/forkyid/go-utils/uuid"
+	"github.com/gin-gonic/gin"
+)
+
+// Auth func
+// return gin.HandlerFunc
+func (DefaultMiddleware) Auth() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		id, err := jwt.ExtractID(context.GetHeader("Authorization"))
+		if err != nil {
+			rest.ResponseMessage(context, http.StatusUnauthorized)
+			context.Abort()
+			return
+		}
+
+		banned, err := isBanned(id, context.GetHeader("Authorization"))
+		if err != nil {
+			logger.LogError(context, "", err.Error())
+		}
+		if banned {
+			rest.ResponseMessage(context, http.StatusForbidden, "Banned")
+			context.Abort()
+			return
+		}
+
+		exists, err := cache.IsCacheExists(
+			fmt.Sprintf(
+				`%v:%v:%v`,
+				aes.Encrypt(id),
+				"whitelist",
+				context.GetHeader("X-Unique-ID"),
+			),
+		)
+		if err != nil {
+			logger.LogError(context, uuid.GetUUID(), "failed on checking if redis key exists: "+err.Error())
+			context.Next()
+			return
+		}
+		if !exists {
+			rest.ResponseMessage(context, http.StatusUnauthorized)
+			context.Abort()
+			return
+		}
+
+		context.Next()
+	}
+}
+
+func isBanned(memberID int, bearer string) (bool, error) {
+	redisKey := fmt.Sprintf(`%v:%v`, memberID, "banned")
+
+	banned, err := cache.IsCacheExists(redisKey)
+	if err != nil {
+		return false, err
+	}
+	if banned {
+		return true, nil
+	}
+
+	_, code := rest.Request{
+		Method: http.MethodGet,
+		URL: fmt.Sprintf(
+			"%v/reports/v1/blocks?blocked_id=%v&block_type_id=%v&blocker_id=0",
+			os.Getenv("API_ORIGIN_URL"),
+			aes.Encrypt(memberID),
+			aes.Encrypt(1)),
+		Headers: map[string]string{
+			"Authorization": bearer,
+		},
+	}.Send()
+	if code != http.StatusOK {
+		return false, fmt.Errorf("auth: get blocked: status not 200")
+	}
+
+	err = cache.SetJSON(redisKey, "", 600)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
---
+++ middleware/go.mod
@@ -0,0 +1,22 @@
+module github.com/heggies/go-utils/middleware
+
+go 1.14
+
+require (
+	github.com/forkyid/go-utils/aes v0.0.0-20201016170839-46d71d0d2b5c
+	github.com/forkyid/go-utils/cache v0.0.0-20201016170839-46d71d0d2b5c
+	github.com/forkyid/go-utils/jwt v0.0.0-20201016170839-46d71d0d2b5c
+	github.com/forkyid/go-utils/logger v0.0.0-20201016170839-46d71d0d2b5c
+	github.com/forkyid/go-utils/rest v0.0.0-20201016170839-46d71d0d2b5c
+	github.com/forkyid/go-utils/uuid v0.0.0-20201016170839-46d71d0d2b5c
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/ugorji/go v1.1.12 // indirect
+	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
+	golang.org/x/sys v0.0.0-20201018230417-eeed37f84f13 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
